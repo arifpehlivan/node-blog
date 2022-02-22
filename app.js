2bcc1986d5fc5523da038461f075ce9eb075313d
arifpehlivan <arifpehlivanapap@gmail.com>
2022-02-22 23:40:03 +0300

    nodejs

---
+++ app.js
@@ -0,0 +1,25 @@
+const path=require("path");
+const express = require('express');
+const {engine}=require("express-handlebars");
+const app = express();
+const port = 3000;
+const hostname="127.0.0.1";
+
+ app.use(express.static("public"));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render("site/index")
+});
+
+app.get('/about', (req, res) => {
+  res.render("site/about")
+});
+
+app.listen(port,hostname, () => {
+  console.log(`Server working, http://${hostname}:${port}/`);
+});
+
